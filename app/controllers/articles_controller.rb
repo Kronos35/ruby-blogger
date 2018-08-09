43e01ad7c882aaa94268c5f25bca176c24d6151a
kronos35 <alvaro@tremak.com>
2018-08-08 20:52:23 -0600

    adding edition feature to the project

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
-  
+
   def index
     # Get all articles
     @articles = Article.all
@@ -20,6 +20,24 @@ class ArticlesController < ApplicationController
     @article.save
     
     # Redirect to see article
+    redirect_to article_path(@article)
+  end
+  def destroy
+    # Delete the entry to the blog
+    @article = Article.destroy(params[:id])
+
+    redirect_to action: "index"
+  end
+  def edit
+    # Find specific Article
+    @article = Article.find(params[:id])
+  end
+  def update
+    # Find article
+    @article = Article.find(params[:id])
+    # Update article's values
+    @article.update(article_params)
+
     redirect_to article_path(@article)
   end
 end
