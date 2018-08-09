3b7008036a015a7d847b364b765a4d89c5f5f988
kronos35 <alvaro@tremak.com>
2018-08-08 20:23:02 -0600

    adding articles helper parameters using "strong parameters" feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,25 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  
   def index
+    # Get all articles
     @articles = Article.all
   end
   def show
+    # Find specific Article
     @article = Article.find(params[:id])
   end
   def new
+    # Setting variable for sharing with the 'New' view
+    @article = Article.new
+  end
+  def create
     
+    # Create new Article
+    @article = Article.new(article_params)
+    @article.save
+    
+    # Redirect to see article
+    redirect_to article_path(@article)
   end
 end
