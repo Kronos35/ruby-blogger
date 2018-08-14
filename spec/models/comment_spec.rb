4878146b4dfc43ba3677886c48a56e30ed2bf61d
kronos35 <alvaro@tremak.com>
2018-08-14 13:06:43 -0600

    Adding more tests

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe Comment, type: :model do 
+  
+  before :each do
+    @comment = Comment.new
+  end
+
+  it "is not valid without an Author" do
+    @comment.author_name = nil
+    @comment.should_not be_valid
+  end
+
+  it "is not valid without a body" do
+    @comment.body = nil
+    @comment.should_not be_valid
+  end
+
+  it "raises an error when saving with no author" do
+    expect{ @comment.save! }.to raise_error
+  end
+end
